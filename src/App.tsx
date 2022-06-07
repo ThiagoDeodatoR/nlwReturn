b6c6f9b1c1404795bb9ce113c7fb7723fd5565d2
Zerfilos <thi_kun@hotmail.com>
2022-06-06 23:10:51 -0300

    NLW Return - 1 Projeto react utilizando vite, tailwind e headlessui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Widget } from "./components/Widget";
+
+export default function App() {
+/* TAILWIND NO HTML 
+  return <button className="bg-purple-700 text-[#FFFFFF] p-2 hover:bg-purple-600 transition-colors">Hello World</button>
+*/
+
+  return <Widget />
+
+}
+
