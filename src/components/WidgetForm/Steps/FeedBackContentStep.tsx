a3e0472f648161f1adc82cdde4f3c42f1ef6a657
Zerfilos <thi_kun@hotmail.com>
2022-06-18 13:48:26 -0300

    Teste

--- src/components/WidgetForm/Steps/FeedBackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedBackContentStep.tsx
@@ -54,7 +54,7 @@ export function FeedBackContentStep({
 
             <form onSubmit={handleFeedbackSubmit} className="my-4 w-full">
                 <textarea 
-                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-1 focus:ring-brand-500 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-1 focus:ring-brand-500 resize-none focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Nos informe o que está acontecendo"  
                     onChange={event => setFeedBackComment(event.target.value)}  
                 />
